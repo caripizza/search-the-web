d67024b8fa7b4f0b88174092f9207dedac4d5b4c
Cari Palazzolo <cari.pizza@gmail.com>
2018-12-03 23:29:40 -0800

    added error message

--- src/components/news/NewsStories.vue
+++ src/components/news/NewsStories.vue
@@ -3,6 +3,10 @@
     <h2>News</h2>
     <NewsSearch :onSearch="handleSearch" :search="search"/>
     <Loader :loading="loading"/>
+    <pre v-show="error" class="error">
+      {{error}}
+    </pre>
+    <p v-if="search">Searching for &quot;{{ search }}&quot;</p>
     <div>
       <ol v-if="news">
         <NewsStory v-for="(newsItem, i) in news"
@@ -26,6 +30,7 @@ export default {
     return {
       news: null,
       loading: false,
+      error: null,
       search: search ? decodeURIComponent(search) : '',
       // search: decodeURIComponent(this.$route.query.search),
       total: 0
@@ -54,13 +59,15 @@ export default {
     },
     searchNews() {
       this.loading = true;
+      this.error = null;
+
       api.getNews(this.search)
         .then(response => {
           this.news = response.articles;
           this.loading = false;
         })
         .catch(err => {
-          console.log(err);
+          this.error = err.message;
           this.loading = false;
         });
     }
--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,14 @@
 export default {
   getNews(searchTerm = '') {
+
+    if(searchTerm.startsWith(undefined)) {
+      return Promise.reject(new Error('There was an error'));
+    }
+
     // eslint-disable-next-line
     const APIKEY = process.env.VUE_APP_APIKEY;
     return fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${APIKEY}&q=${encodeURIComponent(searchTerm)}`)
-    //   .then(response => {
-    //     console.log('fetch complete', response);
-    //   });
 
       .then(response => response.json());
-    //   .then(news => {
-    //     console.log(news);
-    //   });
   }
 };
